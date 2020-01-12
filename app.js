41ac033769f5e583e69bc2109178c259aee5c628
vansanten <vansanten@gmail.com>
2020-01-12 13:48:24 +0100

    Changed mustache to use mst extension instead of mustache. Removed old Jade files.

--- app.js
+++ app.js
@@ -17,22 +17,24 @@ var server = app.listen(app.get('port'), function() {
 });
 
 // Register '.mustache' extension with The Mustache Express
-app.engine('mustache', mustacheExpress());
+// app.engine('mustache', mustacheExpress());
+app.engine('mst', mustacheExpress());
 
 // view engine setup
 // app.set('view engine', 'jade');
-app.set('view engine', 'mustache');
+// app.set('view engine', 'mustache');
+app.set('view engine', 'mst');
 app.set('views', path.join(__dirname, 'views'));
 
 // app.get('/', function(req, res){
   // res.render('index');
 // });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -42,15 +44,15 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
