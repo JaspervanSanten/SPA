0e47547408d2469fc25a664ab60190ac32b6c62f
vansanten <vansanten@gmail.com>
2020-01-12 14:05:17 +0100

    Uncommented logger, etc..

--- app.js
+++ app.js
@@ -21,20 +21,15 @@ var server = app.listen(app.get('port'), function() {
 app.engine('mst', mustacheExpress());
 
 // view engine setup
-// app.set('view engine', 'jade');
 // app.set('view engine', 'mustache');
 app.set('view engine', 'mst');
 app.set('views', path.join(__dirname, 'views'));
 
-// app.get('/', function(req, res){
-  // res.render('index');
-// });
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -44,15 +39,15 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
